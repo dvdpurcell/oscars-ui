433653825279d57468070211abc0dafcf491bd08
David Purcell <dpurcell@lakana.com>
2016-12-21 10:01:53 -0600

    project basics

---
+++ src/main/java/com/supurcell/oscars/OscarsUiApplication.java
@@ -0,0 +1,12 @@
+package com.supurcell.oscars;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OscarsUiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OscarsUiApplication.class, args);
+	}
+}
